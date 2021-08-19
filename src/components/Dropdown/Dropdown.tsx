66aa0de689cce2f41e39e35af93345c424b7445d
ErenYLCN <yalcineren97@gmail.com>
2021-08-19 15:24:26 +0300

    Landing page ui almost finished

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import './Dropdown.scss'
+import React, { useState, useEffect, useRef } from "react";
+import "./Dropdown.scss";
+import chevronDown from "../../assets/chevron-down.png";
 
-export const Dropdown = () => {
-    return (
-        <div>
-            Dropdown
-        </div>
-    )
+interface Props {
+	collection: string;
+	setCollection: React.Dispatch<React.SetStateAction<string>>;
 }
+
+export const Dropdown = ({ collection, setCollection }: Props) => {
+	const [isActive, setIsActive] = useState(false);
+	const collections = [
+		"Featured",
+		"Wallpapers",
+		"Nature",
+		"Textures & Patterns",
+		"Architecture",
+	];
+
+	let dropdownRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		document.addEventListener("mousedown", (event) => {
+			if (dropdownRef.current) {
+				if (!dropdownRef.current.contains(event.target as Node)) {
+					setIsActive(false);
+				}
+			}
+		});
+	});
+
+	return (
+		<div className="dropdown" ref={dropdownRef}>
+			<div
+				className="dropdown-btn"
+				onClick={(e) => setIsActive(!isActive)}
+				style={collection === "Collections" ? { color: "#d5d7e5" } : {}}
+			>
+				<span>{collection}</span>
+				<img src={chevronDown} alt="chevron-down" className="chevron-down" />
+			</div>
+			{isActive && (
+				<div className="dropdown-content">
+					{collections.map((collection) => {
+						return (
+							<div
+								className="dropdown-item"
+								onClick={(e) => {
+									setCollection((e.target as HTMLButtonElement).innerText);
+									setIsActive(false);
+								}}
+							>
+								{collection}
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
+};
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { ChameleonLogo } from '../../components/ChameleonLogo/ChameleonLogo'
-import { Dropdown } from '../../components/Dropdown/Dropdown'
-import './LandingPage.scss'
-
+import React, { useState } from "react";
+import { ChameleonLogo } from "../../components/ChameleonLogo/ChameleonLogo";
+import { Dropdown } from "../../components/Dropdown/Dropdown";
+import "./LandingPage.scss";
 
 export const LandingPage = () => {
-    return (
-        <div className="landing">
-            <div className="container">
-                <ChameleonLogo />
-                <h1><span>image</span> search</h1>
-                <Dropdown />
-                <input />
-            </div>
-        </div>
-    )
-}
+	const [collection, setCollection] = useState<string>("Collections");
+
+	return (
+		<div className="landing">
+			<div className="container">
+				<ChameleonLogo />
+				<h1>
+					<span>image</span> search
+				</h1>
+				<div className="form">
+					<input className="form-text" placeholder="Query" />
+					<Dropdown collection={collection} setCollection={setCollection} />
+					<button className="btn btn-primary">Search</button>
+				</div>
+			</div>
+		</div>
+	);
+};
